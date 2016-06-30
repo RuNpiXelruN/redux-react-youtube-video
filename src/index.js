4ee7e2d9e1643baba25d1b70e36ef010d1d9ea13
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-30 22:10:55 +1000

    Initial commit and basic setup of redux/react search youtube api app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = "AIzaSyAS4zcGhsin75863B6KibQ9WyNJsA-sK9Q";
+
+// Create a new component. This component should
+// produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  ); 
+}
+
+
+
+// Take this component's generated HTML and put
+// it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
